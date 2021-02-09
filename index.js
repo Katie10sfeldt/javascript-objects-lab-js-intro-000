55c041ffcc211b37eefff22f373e6968e01b98a9
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-09 22:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
 
+    return object;
 }
 
 function deleteFromObjectByKey(object, key) {
