6fab0d03611cdfcc915fd8b04a50f03e0c0acbbd
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-09 22:25:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
